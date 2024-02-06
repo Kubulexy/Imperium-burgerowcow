ab7cef29e9d5dde0402ac22e2f22890c7fe32f2f
Kubulexy <150278583+Kubulexy@users.noreply.github.com>
2024-02-06 19:15:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-meme_dict = {
-            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL": "Częsta reakcja na coś zabawnego",
-            "ROFL": "Odpowiedź na żart",
-            "SHEESH": "Lekka dezaprobata",
-            "CREEPY": "Straszny,złowieszczy",
-            "AGGRO": "Stać się agresywnym/zły"
-            }
+import discord
+import time
+import random
+from bot_logic import rzut_moneta
+from discord.ext import commands
 
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Słowo nie zostało znalezione")
+# Zmienna intencje przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def cześć(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def pa(ctx):
+    await ctx.send(f'Do widzenia')
+@bot.command()
+async def rzut(ctx):
+    await ctx.send(f'Zgadnij co wypadnie? ...')
+    time.sleep(1)
+    await ctx.send(f'...')    
+    time.sleep(1)
+    await ctx.send(f'...')
+    time.sleep(1)
+    await ctx.send(f'...')
+    await ctx.send(f'Wynik: ' + rzut_moneta() + '!')      
+
+bot.run("MTE3NjkzMzU4MTYyOTY4NTg0MQ.G_N9rl.aywLC3RPAbyTUjDpaQI2I5iV-2BhUe_CascKlk") 
